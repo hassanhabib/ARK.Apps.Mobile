7cef97355212d1358dfb0b15ae4190ff6d3d01e4
Hassan Habib <hassanhabib@live.com>
2025-05-10 17:23:32 -0700

    ShouldThrowCriticalDependencyExceptionIfCriticalErrorOccurrsAndLogItAsync -> PASS

---
+++ ARK.Apps.Mobile/Services/Foundations/ArkService.Exceptions.cs
@@ -0,0 +1,49 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ARK.Apps.Mobile.Models.Arks;
+using ARK.Apps.Mobile.Models.Arks.Exceptions;
+using RESTFulSense.WebAssembly.Exceptions;
+using Xeptions;
+
+namespace ARK.Apps.Mobile.Services.Foundations
+{
+    internal partial class ArkService : IArkService
+    {
+        private delegate ValueTask<List<Ark>> ReturningArksFunction();
+
+        private async ValueTask<List<Ark>> TryCatch(ReturningArksFunction returningArksFunction)
+        {
+            try
+            {
+                return await returningArksFunction();
+            }
+            catch (HttpResponseUrlNotFoundException httpResponseUrlNotFoundException)
+            {
+                var failedArkConfigurationException =
+                    new FailedArkConfigurationException(
+                        message: "Failed ark configuration error occurred, contact support.",
+                        innerException: httpResponseUrlNotFoundException);
+
+                throw await CreateAndLogCriticalDependencyExceptionAsync(
+                    failedArkConfigurationException);
+            }
+        }
+
+        private async ValueTask<ArkDependencyException> CreateAndLogCriticalDependencyExceptionAsync(
+            Xeption exception)
+        {
+            var arkDependencyException =
+                new ArkDependencyException(
+                    message: "Ark dependency error occurred, contact support.",
+                    innerException: exception);
+
+            await this.loggingBroker.LogCriticalAsync(arkDependencyException);
+
+            return arkDependencyException;
+        }
+    }
+}
