4ef050e8e54e598259a551ba9a217b863ce7c05a
Hassan Habib <hassanhabib@live.com>
2025-05-17 12:27:06 -0700

    ShouldRenderArkViewsAsync -> FAIL

--- ARK.Apps.Mobile.Tests.Units/Components/ArkComponents/ArkComponentTests.Logic.Render.cs
+++ ARK.Apps.Mobile.Tests.Units/Components/ArkComponents/ArkComponentTests.Logic.Render.cs
@@ -2,9 +2,14 @@
 // Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
 // ----------------------------------------------------------------------------------
 
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using ARK.Apps.Mobile.Components.Components;
+using ARK.Apps.Mobile.Models.Views.ArkViews;
 using Bunit;
 using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
 
 namespace ARK.Apps.Mobile.Tests.Units.Components.ArkComponents
 {
@@ -19,6 +24,35 @@ namespace ARK.Apps.Mobile.Tests.Units.Components.ArkComponents
             // then
             defaultArkComponent.ArkViewService.Should().BeNull();
             defaultArkComponent.Carousel.Should().BeNull();
+            this.arkViewServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldRenderArkViewsAsync()
+        {
+            // given
+            List<ArkView> randomArkViews =
+                CreateRandomArkViews();
+
+            List<ArkView> retrievedArkViews =
+                randomArkViews;
+
+            this.arkViewServiceMock.Setup(service =>
+                service.RetrieveAllArkViewsAsync())
+                    .ReturnsAsync(retrievedArkViews);
+
+            // when
+            this.renderedArkComponent =
+                RenderComponent<ArkComponent>();
+
+            // then
+            this.renderedArkComponent.Instance.Carousel
+                .Should().NotBeNull();
+
+            this.arkViewServiceMock.Verify(service =>
+                service.RetrieveAllArkViewsAsync(),
+                    Times.Once);
+
             this.arkViewServiceMock.VerifyNoOtherCalls();
         }
     }
--- ARK.Apps.Mobile.Tests.Units/Components/ArkComponents/ArkComponentTests.cs
+++ ARK.Apps.Mobile.Tests.Units/Components/ArkComponents/ArkComponentTests.cs
@@ -2,12 +2,17 @@
 // Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
 // ----------------------------------------------------------------------------------
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using ARK.Apps.Mobile.Components.Components;
+using ARK.Apps.Mobile.Models.Views.ArkViews;
 using ARK.Apps.Mobile.Services.Views.ArkViews;
 using Bunit;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using Syncfusion.Blazor;
+using Tynamix.ObjectFiller;
 
 namespace ARK.Apps.Mobile.Tests.Units.Components.ArkComponents
 {
@@ -25,6 +30,29 @@ namespace ARK.Apps.Mobile.Tests.Units.Components.ArkComponents
                 this.arkViewServiceMock.Object);
 
             this.Services.AddSyncfusionBlazor();
+        }
+
+        private static List<ArkView> CreateRandomArkViews()
+        {
+            return CreateArkViewFiller()
+                .Create(count: GetRandomNumber())
+                    .ToList();
+        }
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 10).GetValue();
+
+        private static DateTimeOffset GetRandomDateTime() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static Filler<ArkView> CreateArkViewFiller()
+        {
+            var filler = new Filler<ArkView>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(GetRandomDateTime);
+
+            return filler;
         }
     }
 }
