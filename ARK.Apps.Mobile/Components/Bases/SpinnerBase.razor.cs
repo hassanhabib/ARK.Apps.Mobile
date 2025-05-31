97e039124c7c67c6677cf1b05b6ae717033f5770
Abdulsamad Osunlana <osunlanaabdulsamad@gmail.com>
2025-05-31 17:32:46 +0100

    CODE RUB: SpinnerBase

---
+++ ARK.Apps.Mobile/Components/Bases/SpinnerBase.razor.cs
@@ -0,0 +1,12 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components;
+
+namespace ARK.Apps.Mobile.Components.Bases
+{
+    public partial class SpinnerBase : ComponentBase
+    {
+    }
+}
