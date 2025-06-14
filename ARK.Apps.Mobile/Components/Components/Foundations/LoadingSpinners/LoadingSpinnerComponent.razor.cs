76ab09ace39c9df606445bb72c5529852d299b68
Hassan Habib <hassanhabib@live.com>
2025-06-14 08:56:38 -0700

    WIP

---
+++ ARK.Apps.Mobile/Components/Components/Foundations/LoadingSpinners/LoadingSpinnerComponent.razor.cs
@@ -0,0 +1,18 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using ARK.Apps.Mobile.Components.Bases;
+using ARK.Apps.Mobile.Models.Components.Foundations.LoadingSpinners;
+using Microsoft.AspNetCore.Components;
+
+namespace ARK.Apps.Mobile.Components.Components.Foundations.LoadingSpinners
+{
+    public partial class LoadingSpinnerComponent : ComponentBase
+    {
+        public DivisionBase Divison { get; set; }
+        public SpinnerBase Spinner { get; set; }
+        public LoadingSpinnerComponentStyles Styles { get; set; }
+
+    }
+}
