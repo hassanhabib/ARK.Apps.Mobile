1c688a1a1b2e73261160c413d9911de9f379be01
Abdulsamad Osunlana <osunlanaabdulsamad@gmail.com>
2025-06-14 18:00:35 +0100

    ShouldRenderComponentWithStyles -> FAIL

--- ARK.Apps.Mobile.Tests.Units/Components/Foundations/LoadingSpinners/LoadingSpinnerComponentTests.Render.cs
+++ ARK.Apps.Mobile.Tests.Units/Components/Foundations/LoadingSpinners/LoadingSpinnerComponentTests.Render.cs
@@ -2,9 +2,13 @@
 // Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
 // ----------------------------------------------------------------------------------
 
+using System.Collections.Generic;
 using ARK.Apps.Mobile.Components.Components.Foundations.LoadingSpinners;
+using ARK.Apps.Mobile.Models.Components.Foundations.LoadingSpinners;
 using Bunit;
 using FluentAssertions;
+using SharpStyles.Models;
+using SharpStyles.Models.Keyframes;
 
 namespace ARK.Apps.Mobile.Tests.Units.Components.Foundations.LoadingSpinners
 {
@@ -70,6 +74,97 @@ namespace ARK.Apps.Mobile.Tests.Units.Components.Foundations.LoadingSpinners
             this.renderedLoadingSpinnerComponent
                 .Instance.ComponentStyle.Should()
                     .NotBeNull();
+        }
+
+        [Fact]
+        public void ShouldRenderComponentWithStyles()
+        {
+            // given
+            var expectedComponentStyles =
+                new LoadingSpinnerComponentStyles
+                {
+                    RotatedBox = new SharpStyle
+                    {
+                        MarginTop = "300px",
+                        Width = "64px",
+                        Height = "64px",
+                        BackgroundColor = "#f8bbd9",
+                        Transform = "rotate(45deg)",
+                        Animation = "pulse 1s infinite"
+                    },
+
+                    Spinner = new SharpStyle
+                    {
+                        AlignSelf = "center",
+                        JustifyContent = "center"
+                    },
+
+                    SpinnerStroke = new SharpStyle
+                    {
+                        Stroke = "#a855f7",
+                        AnimationDuration = "1s !important"
+                    },
+
+                    Keyframes = new List<SharpKeyframes>
+                    {
+                         new SharpKeyframes
+                         {
+                            Keyframes = new List<SharpKeyframe>
+                            {
+                                new SharpKeyframe
+                                {
+                                    Selector = "0%",
+
+                                    Properties = new List<SharpKeyframeProperty>
+                                    {
+                                        new SharpKeyframeProperty
+                                        {
+                                            Name = "opacity",
+                                            Value = "0.999231"
+                                        }
+                                    }
+                                },
+
+                                new SharpKeyframe
+                                {
+                                    Selector = "50%",
+
+                                    Properties = new List<SharpKeyframeProperty>
+                                    {
+                                        new SharpKeyframeProperty
+                                        {
+                                            Name = "opacity",
+                                            Value = "0.5"
+                                        }
+                                    }
+                                },
+
+                                new SharpKeyframe
+                                {
+                                    Selector = "100%",
+
+                                    Properties = new List<SharpKeyframeProperty>
+                                    {
+                                        new SharpKeyframeProperty
+                                        {
+                                            Name = "opacity",
+                                            Value = "1.0"
+                                        }
+                                    }
+                                }
+                            }
+                         }
+                    }
+                };
+
+            // when
+            this.renderedLoadingSpinnerComponent =
+                RenderComponent<LoadingSpinnerComponent>();
+
+            // then
+            this.renderedLoadingSpinnerComponent.Instance
+                .Styles.Should().BeEquivalentTo(
+                    expectedComponentStyles);
         }
     }
 }
--- ARK.Apps.Mobile/Models/Components/Foundations/LoadingSpinners/LoadingSpinnerComponentStyles.cs
+++ ARK.Apps.Mobile/Models/Components/Foundations/LoadingSpinners/LoadingSpinnerComponentStyles.cs
@@ -3,10 +3,19 @@
 // ----------------------------------------------------------------------------------
 
 using SharpStyles.Models;
+using SharpStyles.Models.Attributes;
 
 namespace ARK.Apps.Mobile.Models.Components.Foundations.LoadingSpinners
 {
     public class LoadingSpinnerComponentStyles : SharpStyle
     {
+        [CssClass]
+        public SharpStyle RotatedBox { get; set; }
+
+        [CssClass]
+        public SharpStyle Spinner { get; set; }
+
+        [CssClass(Selector = ".e-spinner-pane .e-spinner-inner .e-spin-bootstrap5")]
+        public SharpStyle SpinnerStroke { get; set; }
     }
 }
