1e779e4ff93a002515877ee58ca329620b016a36
ZafarUrakov <zafarurakov@outlook.com>
2025-06-15 00:23:33 +0800

    ShouldRenderLoadingSpinnerComponent -> FAIL

--- ARK.Apps.Mobile.Tests.Units/Components/Foundations/LoadingSpinners/LoadingSpinnerComponentTests.Render.cs
+++ ARK.Apps.Mobile.Tests.Units/Components/Foundations/LoadingSpinners/LoadingSpinnerComponentTests.Render.cs
@@ -30,5 +30,42 @@ namespace ARK.Apps.Mobile.Tests.Units.Components.Foundations.LoadingSpinners
             initialLoadingSpinnerComponent.Styles
                 .Should().BeNull();
         }
+
+        [Fact]
+        public void ShouldRenderLoadingSpinnerComponent()
+        {
+            // given
+            string expectedDivisionCssClass = "rotated-box";
+            string expectedSpinnerCssClass = "spinner";
+            bool expectedIsVisibleFlag = true;
+            string expectedSpinnerSize = "32";
+
+            // when
+            this.renderedLoadingSpinnerComponent =
+                RenderComponent<LoadingSpinnerComponent>();
+
+            // then
+            this.renderedLoadingSpinnerComponent
+                .Instance.Divison.Should().NotBeNull();
+
+            this.renderedLoadingSpinnerComponent
+                .Instance.Divison.CssClass.Should()
+                    .Be(expectedDivisionCssClass);
+
+            this.renderedLoadingSpinnerComponent
+                .Instance.Spinner.Should().NotBeNull();
+
+            this.renderedLoadingSpinnerComponent
+                .Instance.Spinner.CssClass.Should()
+                    .Be(expectedSpinnerCssClass);
+
+            this.renderedLoadingSpinnerComponent
+                .Instance.Spinner.IsVisible.Should()
+                    .Be(expectedIsVisibleFlag);
+
+            this.renderedLoadingSpinnerComponent
+                .Instance.Spinner.Size.Should()
+                    .Be(expectedSpinnerSize);
+        }
     }
 }
