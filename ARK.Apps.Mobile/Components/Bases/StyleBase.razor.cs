1372bebce4b68ce9a152ebc15789d2a2b85b2095
Abdulsamad Osunlana <osunlanaabdulsamad@gmail.com>
2025-06-14 16:35:05 +0100

    BASES: StyleBase Component

---
+++ ARK.Apps.Mobile/Components/Bases/StyleBase.razor.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components;
+using SharpStyles.Models;
+
+namespace ARK.Apps.Mobile.Components.Bases
+{
+    public partial class StyleBase
+    {
+        [Parameter]
+        public SharpStyle Styles { get; set; }
+    }
+}
