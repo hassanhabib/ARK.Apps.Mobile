cb5829c17bb97a6bf8523529fdd90bcf38bd49bd
ZafarUrakov <zafarurakov@outlook.com>
2025-06-22 00:29:19 +0800

    ShouldInitializeDefaultValues -> PASS

---
+++ ARK.Apps.Mobile.Tests.Units/Components/Foundations/LoadingDots/LoadingDotsComponentTests.Render.cs
@@ -0,0 +1,40 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using ARK.Apps.Mobile.Components.Components.Foundations.LoadingDots;
+using FluentAssertions;
+
+namespace ARK.Apps.Mobile.Tests.Units.Components.Foundations.LoadingDots
+{
+    public partial class LoadingDotsComponentTests
+    {
+        [Fact]
+        public void ShouldInitializeDefaultValues()
+        {
+            // given . when
+            var initialLoadingDotsComponent =
+                new LoadingDotsComponent();
+
+            // then
+            initialLoadingDotsComponent
+                .LargeHeader.Should().BeNull();
+
+            initialLoadingDotsComponent
+                .Paragraph.Should().BeNull();
+
+            initialLoadingDotsComponent
+                .DotsContainerDivision.Should()
+                    .BeNull();
+
+            initialLoadingDotsComponent
+                .Dot0Division.Should().BeNull();
+
+            initialLoadingDotsComponent
+                .Dot1Division.Should().BeNull();
+
+            initialLoadingDotsComponent
+                .Dot2Division.Should().BeNull();
+        }
+    }
+}
---
+++ ARK.Apps.Mobile/Components/Components/Foundations/LoadingDots/LoadingDotsComponent.razor.cs
@@ -0,0 +1,19 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using ARK.Apps.Mobile.Components.Bases;
+using Microsoft.AspNetCore.Components;
+
+namespace ARK.Apps.Mobile.Components.Components.Foundations.LoadingDots
+{
+    public partial class LoadingDotsComponent : ComponentBase
+    {
+        public LargeHeaderBase LargeHeader { get; set; }
+        public ParagraphBase Paragraph { get; set; }
+        public DivisionBase DotsContainerDivision { get; set; }
+        public DivisionBase Dot0Division { get; set; }
+        public DivisionBase Dot1Division { get; set; }
+        public DivisionBase Dot2Division { get; set; }
+    }
+}
