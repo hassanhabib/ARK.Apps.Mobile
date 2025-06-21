e3c754a4bb8af6c45db14d0dc1a7440882ac3457
ZafarUrakov <zafarurakov@outlook.com>
2025-06-22 00:36:26 +0800

    ShouldRenderLoadingDotsComponent -> FAIL

--- ARK.Apps.Mobile.Tests.Units/Components/Foundations/LoadingDots/LoadingDotsComponentTests.Render.cs
+++ ARK.Apps.Mobile.Tests.Units/Components/Foundations/LoadingDots/LoadingDotsComponentTests.Render.cs
@@ -36,5 +36,71 @@ namespace ARK.Apps.Mobile.Tests.Units.Components.Foundations.LoadingDots
             initialLoadingDotsComponent
                 .Dot2Division.Should().BeNull();
         }
+
+        [Fact]
+        public void ShouldRenderLoadingDotsComponent()
+        {
+            // given
+            string expectedLargeHeaderCssClass =
+                "large-header";
+
+            string expectedParagraphCssClass =
+                "paragraph";
+
+            string expectedDotsContainerCssClass =
+                "loading-dots-container";
+
+            string expectedDot0CssClass = "dot dot-0";
+            string expectedDot1CssClass = "dot dot-1";
+            string expectedDot2CssClass = "dot dot-2";
+
+            // when
+            this.renderedLoadingDotsComponent =
+                RenderComponent<LoadingDotsComponent>();
+
+            // then
+            this.renderedLoadingDotsComponent
+                .Instance.LargeHeader.Should().NotBeNull();
+
+            this.renderedLoadingDotsComponent
+                .Instance.LargeHeader.CssClass
+                    .Should().Be(expectedLargeHeaderCssClass);
+
+            this.renderedLoadingDotsComponent
+                .Instance.Paragraph.Should().NotBeNull();
+
+            this.renderedLoadingDotsComponent
+                .Instance.Paragraph.CssClass
+                    .Should().Be(expectedParagraphCssClass);
+
+            this.renderedLoadingDotsComponent
+                .Instance.DotsContainerDivision
+                    .Should().NotBeNull();
+
+            this.renderedLoadingDotsComponent
+                .Instance.DotsContainerDivision.CssClass
+                    .Should().Be(expectedDotsContainerCssClass);
+
+            this.renderedLoadingDotsComponent
+                .Instance.Dot0Division.Should().NotBeNull();
+
+            this.renderedLoadingDotsComponent
+                .Instance.Dot0Division.CssClass
+                    .Should().Be(expectedDot0CssClass);
+
+            this.renderedLoadingDotsComponent
+                .Instance.Dot1Division.Should().NotBeNull();
+
+            this.renderedLoadingDotsComponent
+                .Instance.Dot1Division.CssClass
+                    .Should().Be(expectedDot1CssClass);
+
+            this.renderedLoadingDotsComponent
+                .Instance.Dot2Division.Should().NotBeNull();
+
+            this.renderedLoadingDotsComponent
+                .Instance.Dot2Division.CssClass
+                    .Should().Be(expectedDot2CssClass);
+        }
     }
 }
