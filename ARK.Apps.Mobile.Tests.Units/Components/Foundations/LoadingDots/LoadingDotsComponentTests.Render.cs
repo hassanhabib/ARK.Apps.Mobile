7530333bcce97b5bdb863ad786e917dad6c096e0
Abdulsamad Osunlana <osunlanaabdulsamad@gmail.com>
2025-06-21 18:33:28 +0100

    ShouldRenderComponentWithStyles -> FAIL

--- ARK.Apps.Mobile.Tests.Units/Components/Foundations/LoadingDots/LoadingDotsComponentTests.Render.cs
+++ ARK.Apps.Mobile.Tests.Units/Components/Foundations/LoadingDots/LoadingDotsComponentTests.Render.cs
@@ -2,8 +2,12 @@
 // Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
 // ----------------------------------------------------------------------------------
 
+using System.Collections.Generic;
 using ARK.Apps.Mobile.Components.Components.Foundations.LoadingDots;
+using ARK.Apps.Mobile.Models.Components.Foundations.LoadingDots;
 using FluentAssertions;
+using SharpStyles.Models;
+using SharpStyles.Models.Keyframes;
 
 namespace ARK.Apps.Mobile.Tests.Units.Components.Foundations.LoadingDots
 {
@@ -123,6 +127,109 @@ namespace ARK.Apps.Mobile.Tests.Units.Components.Foundations.LoadingDots
             this.renderedLoadingDotsComponent
                 .Instance.Dot2Division.CssClass
                     .Should().Be(expectedDot2CssClass);
+        }
+
+        [Fact]
+        public void ShouldRenderComponentWithStyles()
+        {
+            // given
+            var expectedComponentStyle =
+                new LoadingDotsComponentStyle
+                {
+                    LargeHeader = new SharpStyle
+                    {
+                        FontSize = "20px",
+                        FontWeight = "600",
+                        Color = "rgb(31, 41, 55)",
+                        MarginBottom = "24px"
+                    },
+
+                    Paragraph = new SharpStyle
+                    {
+                        Color = "rgb(107, 114, 128)",
+                        FontSize = "14px",
+                        MarginBottom = "24px"
+                    },
+
+                    DotsContainerDivision = new SharpStyle
+                    {
+                        Display = "flex",
+                        JustifyContent = "center",
+                        Gap = "4px"
+                    },
+
+                    Dot = new SharpStyle
+                    {
+                        Width = "8px",
+                        Height = "8px",
+                        Background = "#d8b4fe",
+                        BorderRadius = "50%",
+                        Animation = "bounce 1.4s infinite ease-in-out"
+                    },
+
+                    Dot0Division = new SharpStyle
+                    {
+                        AnimationDelay = "0ms"
+                    },
+
+                    Dot1Division = new SharpStyle
+                    {
+                        AnimationDelay = ".15s"
+                    },
+
+                    Dot2Division = new SharpStyle
+                    {
+                        AnimationDelay = ".3s"
+                    },
+
+                    Keyframes = new List<SharpKeyframes>
+                    {
+                         new SharpKeyframes
+                         {
+                            Name = "bounce",
+
+                            Keyframes = new List<SharpKeyframe>
+                            {
+                                new SharpKeyframe
+                                {
+                                    Selector = "0%, 80% 100%",
+
+                                    Properties = new List<SharpKeyframeProperty>
+                                    {
+                                        new SharpKeyframeProperty
+                                        {
+                                            Name = "transform",
+                                            Value = "scale(0)"
+                                        }
+                                    }
+                                },
+
+                                new SharpKeyframe
+                                {
+                                    Selector = "40%",
+
+                                    Properties = new List<SharpKeyframeProperty>
+                                    {
+                                        new SharpKeyframeProperty
+                                        {
+                                            Name = "transform",
+                                            Value = "scale(1)"
+                                        }
+                                    }
+                                }
+                            }
+                         }
+                    }
+                };
+
+            // when
+            this.renderedLoadingDotsComponent =
+                RenderComponent<LoadingDotsComponent>();
+
+            // then
+            this.renderedLoadingDotsComponent.Instance
+                .Style.Should().BeEquivalentTo(
+                    expectedComponentStyle);
         }
     }
 }
--- ARK.Apps.Mobile/Models/Components/Foundations/LoadingDots/LoadingDotsComponentStyle.cs
+++ ARK.Apps.Mobile/Models/Components/Foundations/LoadingDots/LoadingDotsComponentStyle.cs
@@ -3,10 +3,31 @@
 // ----------------------------------------------------------------------------------
 
 using SharpStyles.Models;
+using SharpStyles.Models.Attributes;
 
 namespace ARK.Apps.Mobile.Models.Components.Foundations.LoadingDots
 {
     public class LoadingDotsComponentStyle : SharpStyle
     {
+        [CssClass(Selector = ".paragraph")]
+        public SharpStyle Paragraph { get; set; }
+
+        [CssClass(Selector = ".large-header")]
+        public SharpStyle LargeHeader { get; set; }
+
+        [CssClass(Selector = ".loading-dots-container")]
+        public SharpStyle DotsContainerDivision { get; set; }
+
+        [CssClass(Selector = ".dot")]
+        public SharpStyle Dot { get; set; }
+
+        [CssClass(Selector = ".dot-0")]
+        public SharpStyle Dot0Division { get; set; }
+
+        [CssClass(Selector = ".dot-1")]
+        public SharpStyle Dot1Division { get; set; }
+
+        [CssClass(Selector = ".dot-2")]
+        public SharpStyle Dot2Division { get; set; }
     }
 }
