c604baa4d682f63766551677d6120b369cdc8268
Abdulsamad Osunlana <osunlanaabdulsamad@gmail.com>
2025-06-21 18:13:52 +0100

    ShouldRenderLoadingDotsComponent -> PASS

--- ARK.Apps.Mobile/Components/Components/Foundations/LoadingDots/LoadingDotsComponent.razor.cs
+++ ARK.Apps.Mobile/Components/Components/Foundations/LoadingDots/LoadingDotsComponent.razor.cs
@@ -3,6 +3,7 @@
 // ----------------------------------------------------------------------------------
 
 using ARK.Apps.Mobile.Components.Bases;
+using ARK.Apps.Mobile.Models.Components.Foundations.LoadingDots;
 using Microsoft.AspNetCore.Components;
 
 namespace ARK.Apps.Mobile.Components.Components.Foundations.LoadingDots
@@ -10,11 +11,17 @@ namespace ARK.Apps.Mobile.Components.Components.Foundations.LoadingDots
     public partial class LoadingDotsComponent : ComponentBase
     {
         public StyleBase ComponentStyle { get; set; }
+        public LoadingDotsComponentStyle Style { get; set; }
         public LargeHeaderBase LargeHeader { get; set; }
         public ParagraphBase Paragraph { get; set; }
         public DivisionBase DotsContainerDivision { get; set; }
         public DivisionBase Dot0Division { get; set; }
         public DivisionBase Dot1Division { get; set; }
         public DivisionBase Dot2Division { get; set; }
+
+        protected override void OnInitialized()
+        {
+            this.Style = new LoadingDotsComponentStyle();
+        }
     }
 }
